54a06538981db6699b033dda7933613a2bf8bb03
B-ctrl <58861435+B-ctrl@users.noreply.github.com>
2020-02-13 14:30:40 -0500

    cleaned up date coding and added 2 tickemaster events

--- myScript.js
+++ myScript.js
@@ -1,47 +1,45 @@
 
-var invoiceDate = document.querySelector("#invoiceDate").value;
-var days = Number(document.querySelector("#days").value);
-var dueDateElement = document.querySelector("#dueDate");
-var initialDepartureDate = document.querySelector("#invoiceDate").value;
+var arrivalDate = document.querySelector("#arrivalDate").value;
+var days = 5;
 var day1;
 var day2;
 var day3;
 var day4;
 var day5;
 var eventDays = [];
+var day1Formatted = "2020-02-28";   // hardcoded for now
+var day2Formatted = "2020-02-29";   // hardcoded for now
+var day3Formatted = "2020-03-01";   // hardcoded for now
+var day4Formatted = "2020-02-02";   // hardcoded for now
+var day5Formatted = "2020-02-03";   // hardcoded for now
+var latlon = "38.8951,-77.0364";    // hardcoded for now
+var Day1Events = {};
+var Day2Events = {};
+var Day3Events = {};
+var Day4Events = {};
+var Day5Events = {};
 
-  document.querySelector("#addDays").addEventListener("click", function() {
-    invoiceDate = document.querySelector("#invoiceDate").value;
-    days = Number(document.querySelector("#days").value);
-    dueDateElement = document.querySelector("#dueDate");
-    initialDepartureDate = document.querySelector("#invoiceDate").value;
+
+document.querySelector("#submitRequest").addEventListener("click", function() {
+   arrivalDate = document.querySelector("#arrivalDate").value;
   
-        if (!isNaN(days) && invoiceDate.length) {
-      //  console.log("1st invoice date: " + invoiceDate);
-        invoiceDate = invoiceDate.split("-");
-       // console.log("invoice date after split " + invoiceDate);
-        invoiceDate = new Date(invoiceDate[0], invoiceDate[1] - 1, invoiceDate[2]);
-       // console.log("invoice date after new date: " + invoiceDate);
-            
-            console.log("invoice date: " + invoiceDate);
-            day1 = invoiceDate.toString();
-            console.log(" Day 1: " + day1);
+    if (!isNaN(days) && arrivalDate.length) {
+      arrivalDate = arrivalDate.split("-");
+      arrivalDate = new Date(arrivalDate[0], arrivalDate[1] - 1, arrivalDate[2]);
+  
+            day1 = arrivalDate.toString();
 
-            invoiceDate.setDate(invoiceDate.getDate() + 1);            
-            day2 = invoiceDate.toString();
-            console.log(" Day 2: " + day2);
+            arrivalDate.setDate(arrivalDate.getDate() + 1);            
+            day2 = arrivalDate.toString();
 
-            invoiceDate.setDate(invoiceDate.getDate() + 1);            
-            day3 = invoiceDate.toString();
-            console.log(" Day 3: " + day3);
+            arrivalDate.setDate(arrivalDate.getDate() + 1);            
+            day3 = arrivalDate.toString();
 
-            invoiceDate.setDate(invoiceDate.getDate() + 1);            
-            day4 = invoiceDate.toString();
-            console.log(" Day 4: " + day4);
+            arrivalDate.setDate(arrivalDate.getDate() + 1);            
+            day4 = arrivalDate.toString();
 
-            invoiceDate.setDate(invoiceDate.getDate() + 1);            
-            day5 = invoiceDate.toString();
-            console.log(" Day 5: " + day5);
+            arrivalDate.setDate(arrivalDate.getDate() + 1);            
+            day5 = arrivalDate.toString();
             
             console.log(" day 1 event date after 5 rounds: " + day1);
             console.log(" day 2 event date after 5 rounds: " + day2);
@@ -51,8 +49,49 @@ var eventDays = [];
 
             eventDays = [day1, day2, day3, day4, day5];
             console.log("event days array " + eventDays);
+    }
 
-           
+    showPosition();
 
+});
+
+function showPosition() {
+
+  // get events for day 1 and store in Day1Events
+    $.ajax({
+        type: "GET",
+        url: "https://app.ticketmaster.com/discovery/v2/events?apikey=azaUxsQPC2NvNoM7ZPpJOHJ0xw2N0iqd&radius=10&unit=miles&locale=*&size=5&countryCode=US&localStartDateTime=" + day1Formatted + "T00:00:00," + day1Formatted + "T23:59:59&geoPoint=" + latlon,
+        async: true,
+        dataType: "json",
+        success: function (Day1JSON) {
+          Day1Events = Day1JSON;
+          },
+        error: function (xhr, status, err) {
+          console.log(err);
+          }
+      });
+              
+  // get events for day 2 and store in Day2Events
+    $.ajax({
+      type: "GET",
+      url: "https://app.ticketmaster.com/discovery/v2/events?apikey=azaUxsQPC2NvNoM7ZPpJOHJ0xw2N0iqd&radius=10&unit=miles&locale=*&size=5&countryCode=US&localStartDateTime=" + day2Formatted + "T00:00:00," + day2Formatted + "T23:59:59&geoPoint=" + latlon,
+      async: true,
+      dataType: "json",
+      success: function (Day2JSON) {
+        Day2Events = Day2JSON;
+        },
+      error: function (xhr, status, err) {
+        console.log(err);
         }
-  });
+    });
+            
+
+
+  console.log("Day 1 Events: ");
+  console.log(Day1Events);
+  console.log("Day 2 Events: ")
+  console.log(Day2Events);   
+
+}
+
+
